051ba989015f63e44a402ebf9550a439569c1d84
blackprince001 <appiahboaduprince@gmail.com>
2023-02-14 23:07:38 +0000

    This is what it feels to come from a dynamically typed language

--- exercises/exercise_four.cpp
+++ exercises/exercise_four.cpp
@@ -4,29 +4,30 @@
 #include <string>
 #include <vector>
 
+template <typename OptionType>
 class QuestionOption {
-    std::any option;
+    OptionType option;
     bool is_correct;
 
    public:
-    QuestionOption(void* answer, bool is_correct_option)
+    QuestionOption(OptionType answer, bool is_correct_option)
         : option(answer), is_correct(is_correct_option) {}
 
-    std::any get_option() { return option; }
+    OptionType get_option() { return option; }
 };
 
 class Question {
     std::string question_statement;
-    std::vector<QuestionOption> options;
+    std::vector<QuestionOption<std::any>> options;
 
    public:
     Question(std::string question) : question_statement(question){};
-    void create_option(std::string new_option, bool is_correct_option) {
-        QuestionOption opt(&new_option, is_correct_option);
+    void create_option(const std::any new_option, bool is_correct_option) {
+        QuestionOption opt(new_option, is_correct_option);
         options.push_back(opt);
     }
     std::string get_question_statement() { return question_statement; }
-    std::vector<QuestionOption> get_options() { return options; }
+    std::vector<QuestionOption<std::any>> get_options() { return options; }
 };
 
 class MakeQuiz {
@@ -35,17 +36,17 @@ class MakeQuiz {
    public:
     void add_question(Question& quest) { questions.push_back(quest); }
     void show_questions() {
-        for (auto question : questions) {
+        for (auto& question : questions) {
             std::cout << question.get_question_statement() << "\n";
             for (auto option : question.get_options()) {
-                std::cout << option.get_option() << "\n";
+                // something happens here
+                std::any caught = option.get_option();
+                // std::cout << std::any(caught);  // cant print the options
+                // because we cannot infer the type from this scope
             }
         }
     }
 };
-
-// Not the most efficient or elegant solution but this one works for the
-// meantime.
 
 int main() {
     Question question1("What is the answer for the Question?");
