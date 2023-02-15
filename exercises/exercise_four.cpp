c784a20c7c41cd88335b01e3b86c61e575099795
blackprince001 <appiahboaduprince@gmail.com>
2023-02-15 13:09:24 +0000

    Rewrite quiz modeling object structure

--- exercises/exercise_four.cpp
+++ exercises/exercise_four.cpp
@@ -1,33 +1,32 @@
 // rewrite the quiz class template described in the slides.
-#include <any>
 #include <iostream>
 #include <string>
+#include <variant>
 #include <vector>
 
-template <typename OptionType>
-class QuestionOption {
-    OptionType option;
+struct QuestionOption {
+    std::variant<float, int, std::string> option;
     bool is_correct;
 
    public:
-    QuestionOption(OptionType answer, bool is_correct_option)
+    QuestionOption(auto answer, bool is_correct_option)
         : option(answer), is_correct(is_correct_option) {}
 
-    OptionType get_option() { return option; }
+    auto get_option() { return option; }
 };
 
 class Question {
     std::string question_statement;
-    std::vector<QuestionOption<std::any>> options;
+    std::vector<QuestionOption> options;
 
    public:
     Question(std::string question) : question_statement(question){};
-    void create_option(const std::any new_option, bool is_correct_option) {
+    void create_option(const auto new_option, bool is_correct_option) {
         QuestionOption opt(new_option, is_correct_option);
         options.push_back(opt);
     }
     std::string get_question_statement() { return question_statement; }
-    std::vector<QuestionOption<std::any>> get_options() { return options; }
+    std::vector<QuestionOption> get_options() { return options; }
 };
 
 class MakeQuiz {
@@ -39,10 +38,14 @@ class MakeQuiz {
         for (auto& question : questions) {
             std::cout << question.get_question_statement() << "\n";
             for (auto option : question.get_options()) {
-                // something happens here
-                std::any caught = option.get_option();
-                // std::cout << std::any(caught);  // cant print the options
-                // because we cannot infer the type from this scope
+                auto caught = option.get_option();
+                try {
+                    std::cout << std::get<std::string>(caught) << "\n" ||
+                        std::cout << std::get<float>(caught) << "\n" ||
+                        std::cout << std::get<int>(caught) << "\n";
+                } catch (const std::bad_variant_access& e) {
+                    continue;
+                }
             }
         }
     }
@@ -51,6 +54,8 @@ class MakeQuiz {
 int main() {
     Question question1("What is the answer for the Question?");
     question1.create_option("std::string new_option", false);
+    question1.create_option(23, false);
+    question1.create_option(543.4f, false);
 
     MakeQuiz quiz1;
     quiz1.add_question(question1);
