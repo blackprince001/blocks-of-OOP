265fafe65db92e92831a933064d926ab45a97a70
blackprince001 <appiahboaduprince@gmail.com>
2023-02-15 14:52:54 +0000

    the greater forces won this battle, but i wont lose the war

--- exercises/exercise_four.cpp
+++ exercises/exercise_four.cpp
@@ -1,18 +1,20 @@
 // rewrite the quiz class template described in the slides.
+#include <any>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <variant>
 #include <vector>
 
 struct QuestionOption {
-    std::variant<float, int, std::string> option;
+    std::string option;
     bool is_correct;
 
    public:
-    QuestionOption(auto answer, bool is_correct_option)
+    QuestionOption(std::string answer, bool is_correct_option)
         : option(answer), is_correct(is_correct_option) {}
 
-    auto get_option() { return option; }
+    std::string get_option() { return option; }
 };
 
 class Question {
@@ -21,7 +23,7 @@ class Question {
 
    public:
     Question(std::string question) : question_statement(question){};
-    void create_option(const auto new_option, bool is_correct_option) {
+    void create_option(std::string new_option, bool is_correct_option) {
         QuestionOption opt(new_option, is_correct_option);
         options.push_back(opt);
     }
@@ -33,20 +35,12 @@ class MakeQuiz {
     std::vector<Question> questions;
 
    public:
-    void add_question(Question& quest) { questions.push_back(quest); }
+    void add_question(Question &quest) { questions.push_back(quest); }
     void show_questions() {
-        for (auto& question : questions) {
+        for (auto &question : questions) {
             std::cout << question.get_question_statement() << "\n";
-            for (auto option : question.get_options()) {
-                auto caught = option.get_option();
-                try {
-                    std::cout << std::get<std::string>(caught) << "\n" ||
-                        std::cout << std::get<float>(caught) << "\n" ||
-                        std::cout << std::get<int>(caught) << "\n";
-                } catch (const std::bad_variant_access& e) {
-                    continue;
-                }
-            }
+            for (auto option : question.get_options())
+                std::cout << option.get_option() << "\n";
         }
     }
 };
@@ -54,8 +48,8 @@ class MakeQuiz {
 int main() {
     Question question1("What is the answer for the Question?");
     question1.create_option("std::string new_option", false);
-    question1.create_option(23, false);
-    question1.create_option(543.4f, false);
+    question1.create_option("23", false);
+    question1.create_option("411.2f", false);
 
     MakeQuiz quiz1;
     quiz1.add_question(question1);
