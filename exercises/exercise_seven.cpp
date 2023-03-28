e3539cc74c09994b673ad143ad3f5d3603932b0f
blackprince001 <appiahboaduprince@gmail.com>
2023-03-28 00:26:24 +0000

    Add `this` implicit parameter keyword exercise

---
+++ exercises/exercise_seven.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <valarray>
+
+// implicit parameter `this` and how it maps attributes in all scopes.
+// analogous to the `self` keyword in other languages like python.
+/*
+class A:
+    def __init__(self, val: int):
+        self._some_param = val
+
+    def change_internal_params(self):
+        self._some_param += 1
+*/
+class mainObj {
+    int some_property = 0;
+
+   public:
+    void change_said_property() {
+        int *pProp = &this->some_property;
+        *(pProp) += 1;
+    }
+    int get_said_property() { return some_property; }
+};
+
+int main() {
+    mainObj someObj;
+
+    for (int i = 0; i < 10; ++i) {
+        someObj.change_said_property();
+    }
+    std::cout << someObj.get_said_property();
+}
