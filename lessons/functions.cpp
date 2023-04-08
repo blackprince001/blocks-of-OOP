51634363ea2a3c0260bee0b9241c38daa31bf413
blackprince001 <appiahboaduprince@gmail.com>
2023-04-08 20:36:05 +0000

    fix logic and typos in concept source files

--- lessons/functions.cpp
+++ lessons/functions.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
 void replchar(const char chin, int ntimes) {
-    for (int i = 0; i < ntimes; ++i) std::cout << chin;
+  for (int i = 0; i < ntimes; ++i) std::cout << chin;
 }
 
 struct Distance {
-    double x;
-    double y;
+  double x;
+  double y;
 
-    // Constructor overloading
-    // since we load overloading everything in cpp
-    // we can overload constructors; and in this example
-    // we overload the Distance constructor to take values when there are some
-    // and ZEROS when there are None;
-    Distance() : x(0), y(0) {}
-    Distance(double x, double y) : x(x), y(y) {}
+  // Constructor overloading
+  // since we load overloading everything in cpp
+  // we can overload constructors; and in this example
+  // we overload the Distance constructor to take values when there are some
+  // and ZEROS when there are None;
+  Distance() : x(0), y(0) {}
+  Distance(double x, double y) : x(x), y(y) {}
 };
 
 Distance NullDistance;  // Reference objects are made global so that you can
@@ -22,24 +22,24 @@ Distance NullDistance;  // Reference objects are made global so that you can
 // as seen in line 40
 
 Distance add_distances(Distance &pos_1, Distance &pos_2) {
-    Distance ref;
-    // whatever weird math that is going on here!
-    ref.x = (pos_1.x + pos_2.x);
-    ref.y = (pos_1.y + pos_2.y);
+  Distance ref;
+  // whatever weird math that is going on here!
+  ref.x = (pos_1.x + pos_2.x);
+  ref.y = (pos_1.y + pos_2.y);
 
-    return ref;
+  return ref;
 }
 
 inline double area_of_circle(double radius) {
-    return (3.14159 * (radius * radius));
+  return (3.14159 * (radius * radius));
 }
 
 double magnitude_from_origin(Distance ref) {
-    return (ref.x * ref.x) + (ref.y * ref.y);
+  return (ref.x * ref.x) + (ref.y * ref.y);
 }
 
 Distance &reset_distance() {
-    return NullDistance;  // references whatever values NullDistance has
+  return NullDistance;  // references whatever values NullDistance has
 }
 
 Distance change_cordinates(
@@ -50,49 +50,49 @@ Distance dont_change_cordinates(
 // no modification of the Distance object referenced
 
 int operation(int x, int y, int (*Callable)(int, int)) {
-    // pointer functions as arguments
-    return Callable(x, y);
+  // pointer functions as arguments
+  return Callable(x, y);
 }
 
 int addition(int a, int b) { return a + b; }
 
 int main() {
-    // pass by value
-    replchar('*', 45);
+  // pass by value
+  replchar('*', 45);
 
-    char dash = '-';
-    int nloops = 15;
+  char dash = '-';
+  int nloops = 15;
 
-    // pass by value but with variables
-    replchar(dash, nloops);
+  // pass by value but with variables
+  replchar(dash, nloops);
 
-    // is the backslash a character? Yes!
-    replchar('\n', 20);  // Printing a lot of empty spaces
+  // is the backslash a character? Yes!
+  replchar('\n', 20);  // Printing a lot of empty spaces
 
-    Distance kyei_location(12.31, 13.8);
-    std::cout << "Resultant: " << magnitude_from_origin(kyei_location);
+  Distance kyei_location(12.31, 13.8);
+  std::cout << "Resultant: " << magnitude_from_origin(kyei_location);
 
-    Distance KK_location(24, 25);
-    Distance Resultant =
-        add_distances(kyei_location, KK_location);  // pass by reference
-    std::cout << "Resultant: " << magnitude_from_origin(Resultant);
+  Distance KK_location(24, 25);
+  Distance Resultant =
+      add_distances(kyei_location, KK_location);  // pass by reference
+  std::cout << "Resultant: " << magnitude_from_origin(Resultant);
 
-    Distance origin;
+  Distance origin;
 
-    Distance origin_to_kyei = add_distances(kyei_location, origin);
-    std::cout << "Resultant: "
-              << magnitude_from_origin(
-                     origin_to_kyei);  // should be basically finding the value
-                                       // for the mag of Kyei
+  Distance origin_to_kyei = add_distances(kyei_location, origin);
+  std::cout << "Resultant: "
+            << magnitude_from_origin(
+                   origin_to_kyei);  // should be basically finding the value
+                                     // for the mag of Kyei
 
-    kyei_location = reset_distance();  // we reference the NullDistance values
-                                       // to kyei_location
-    // this also means we can alter the values of NullDistance when we move it
-    // to the lHS. `reset_distance = some distance object with valid values;`
-    std::cout << "Resultant: " << magnitude_from_origin(kyei_location);
+  kyei_location = reset_distance();  // we reference the NullDistance values
+                                     // to kyei_location
+  // this also means we can alter the values of NullDistance when we move it
+  // to the lHS. `reset_distance = some distance object with valid values;`
+  std::cout << "Resultant: " << magnitude_from_origin(kyei_location);
 
-    int res = operation(12, 45, addition);
-    std::cout << res;
+  int res = operation(12, 45, addition);
+  std::cout << res;
 
-    return 0;
+  return 0;
 }
